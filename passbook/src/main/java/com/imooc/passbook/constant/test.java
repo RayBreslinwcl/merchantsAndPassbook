010f06d873bc2e68ab7dddd0f500c20a451d2362
Raywcl <2728928100@qq.com>
2020-11-22 16:38:14 +0800

    商户-用户系统初始化

---
+++ passbook/src/main/java/com/imooc/passbook/constant/test.java
@@ -0,0 +1,12 @@
+package com.imooc.passbook.constant;
+
+/**
+ * Created by Administrator on 2019/10/12.
+ */
+public class test {
+    public static void main(String[] args) {
+        FeedbackType ft=FeedbackType.APP;
+
+        FeedbackType ft2=FeedbackType.PASS;
+    }
+}
